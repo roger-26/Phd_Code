d9b79b2d55b307f48978fd0295c9a02fff8bdb30
roger-26 <roger10101@example.com>
2019-06-16 15:58:50 -0500

    ubuntuj16

--- ConvertingAllVIdeos_Folder2YCbCr_Ubuntu.m
+++ ConvertingAllVIdeos_Folder2YCbCr_Ubuntu.m
@@ -1,22 +1,27 @@
 clc;
 close all;
 clear all;
-
+tic
 %This code converts all AVI files in one folder to MSCN format. Must be executed in the folder where
 %you want to save the videos in MSCN Format. This save the videos in format uncompressed AVI
 %Roger Gomez Nieto - Created: June 14 2019
-cd '/media/javeriana/VISION_2/datasets/LIVE-Qualcomm/Exposure/'
+Folder_Containing_Videos = ...
+    '/media/javeriana/VISION_2/datasets/LIVE-Qualcomm/stabilization/'; 
+cd '/media/javeriana/VISION_2/datasets/LIVE-Qualcomm/stabilization/'
 %Fetching all names from AVI files in folder
 Videos_Inside_Folder = dir('*.avi');
-cd '/media/javeriana/VISION_EXT/Datasets/LIVE-Qualcomm_MSCN/Exposure/'
+cd '/media/javeriana/VISION_EXT/Datasets/LIVE-Qualcomm_ycbcr/stabilization/'
 addpath('/home/javeriana/roger_gomez/Phd_Code/')
 %THis is the folder where are the videos in AVI Format. This can be different to folder to save MSCN
 %videos 
 
-Folder_Containing_Videos = '/media/javeriana/VISION_2/datasets/LIVE-Qualcomm/Exposure/'; 
+
 Number_Of_Videos = length(Videos_Inside_Folder)
 for i=1: Number_Of_Videos
+    tic
     Current_Video_to_Process=    Videos_Inside_Folder(i).name
-    [successful_conversion]=avi2mscn(Current_Video_to_Process,Folder_Containing_Videos)
+    [successful_conversion]=avi2ycbcr(Current_Video_to_Process,Folder_Containing_Videos)
     i
+    toc
 end
+toc
--- Converting_Allvideos_2MSCN_Ubuntu.m
+++ Converting_Allvideos_2MSCN_Ubuntu.m
@@ -1,19 +1,21 @@
 clc;
 close all;
 clear all;
-
+tic
 %This code converts all AVI files in one folder to MSCN format. Must be executed in the folder where
 %you want to save the videos in MSCN Format. This save the videos in format uncompressed AVI
 %Roger Gomez Nieto - Created: June 14 2019
-cd '/media/javeriana/VISION_2/datasets/LIVE-Qualcomm/Exposure/'
+Folder_Containing_Videos = ...
+    '/media/javeriana/VISION_2/datasets/LIVE-Qualcomm/stabilization/'; 
+cd '/media/javeriana/VISION_2/datasets/LIVE-Qualcomm/stabilization/'
 %Fetching all names from AVI files in folder
 Videos_Inside_Folder = dir('*.avi');
-cd '/media/javeriana/VISION_EXT/Datasets/LIVE-Qualcomm_MSCN/Exposure/'
+cd '/media/javeriana/VISION_EXT/Datasets/LIVE-Qualcomm_MSCN/stabilization/'
 addpath('/home/javeriana/roger_gomez/Phd_Code/')
 %THis is the folder where are the videos in AVI Format. This can be different to folder to save MSCN
 %videos 
 
-Folder_Containing_Videos = '/media/javeriana/VISION_2/datasets/LIVE-Qualcomm/Exposure/'; 
+
 Number_Of_Videos = length(Videos_Inside_Folder)
 for i=1: Number_Of_Videos
     tic
@@ -22,3 +24,4 @@ for i=1: Number_Of_Videos
     i
     toc
 end
+toc
