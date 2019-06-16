64189530b8a49e5a435056b872e57847dcccaca4
Roger G Nieto <rogergomez@utp.edu.co>
2019-06-16 14:07:35 -0500

    j16_1

---
+++ avi2ycbcr.m
@@ -0,0 +1,41 @@
+function [successful_conversion]=avi2ycbcr(Name_Video,path2folder)
+% This code converts a RGB video in AVI format to YCbCr (Mean Substracted Contrast Normalization
+% Format)
+% Roger Gomez Nieto -  June 12 2019
+
+% Read the AVI file
+% addpath('C:\Users\DeepLearning_PUJ\Documents\javeriana')
+addpath(path2folder);
+% Name_Video='0723_ManUnderTree_GS5_03_20150723_130016.avi'
+
+vid1=VideoReader(Name_Video)
+Number_Frames_Video=vid1.NumberOfFrames;
+
+ Full_Name_Divided= strsplit(Name_Video,'.');
+    %To extract only the name, without the point from extension
+    Only_Name_NoExt= char(Full_Name_Divided(1));
+namevideo_msnc=strcat(Only_Name_NoExt,'_mscn');
+
+writerObj1 = VideoWriter(namevideo_msnc,'Uncompressed AVI');
+
+
+writerObj1.FrameRate=vid1.FrameRate;
+open(writerObj1);
+Number_Of_Frames=vid1.NumberOfFrames
+for i = 1 : vid1.NumberOfFrames
+    im=read(vid1,i);
+
+    %obtaining ycbcrrepresentation
+    YCBCR=rgb2ycbcr(im);
+    imgOut=normalize(YCBCR,'range');
+    
+    
+    writeVideo(writerObj1,imgOut);
+    if mod(i,100)==0
+    i
+    end
+end
+close(writerObj1)
+successful_conversion=1;
+
+%gato
