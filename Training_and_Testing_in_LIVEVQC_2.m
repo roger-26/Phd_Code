c56aa877dc8667ea8f96d97f9a7ddbf2f63bcd32
roger-26 <roger10101@example.com>
2019-09-18 09:08:45 -0500

    sep18_ServerJaveriana

--- Training_and_Testing_in_LIVEVQC_2.m
+++ Training_and_Testing_in_LIVEVQC_2.m
@@ -2,23 +2,30 @@ clc;
 close all;
 clear all;
 
-%con este código entreno en la LIVE-VQC y pruebo con la misma base de datos. Usando un esquema de
+%con este cï¿½digo entreno en la LIVE-VQC y pruebo con la misma base de datos. Usando un esquema de
 %80-20.
 
 Training_Percentage = 0.8;
 Test_Percentaje = 0.2;
-
+SVR_Iterations = 350;
 
 %loading data from LIVEVQC dataset
 % data_LIVEVQC= load('G:\datasets\LIVEVQCPrerelease\LIVE_VQC_YCbCr_8Frames_C3D_Features\data_LIVEVQC.mat');
 % MOS_LIVEVQC= load('G:\datasets\LIVEVQCPrerelease\LIVE_VQC_YCbCr_8Frames_C3D_Features\MOS_LIVEVQC.mat');
-data_LIVEVQC= load('C:\Dropbox\Javeriana\current_work\LIVEVQC_TestVQA\data_LIVEVQC.mat');
-MOS_LIVEVQC= load('C:\Dropbox\Javeriana\current_work\LIVEVQC_TestVQA\MOS_LIVEVQC.mat');
+
+
+% data_LIVEVQC= load('C:\Dropbox\Javeriana\current_work\LIVEVQC_TestVQA\data_LIVEVQC.mat');
+% MOS_LIVEVQC= load('C:\Dropbox\Javeriana\current_work\LIVEVQC_TestVQA\MOS_LIVEVQC.mat');
+% data_LIVEVQC=data_LIVEVQC.LIVEVQC_Data_Averaged;
+% MOS_LIVEVQC = MOS_LIVEVQC.MOS_LIVE_VQC;
+
+data_LIVEVQC = load('/home/javeriana/roger_gomez/Dropbox/Javeriana/current_work/LIVEVQC_TestVQA/data_LIVEVQC.mat');
+MOS_LIVEVQC =  load('/home/javeriana/roger_gomez/Dropbox/Javeriana/current_work/LIVEVQC_TestVQA/MOS_LIVEVQC.mat');
 data_LIVEVQC=data_LIVEVQC.LIVEVQC_Data_Averaged;
 MOS_LIVEVQC = MOS_LIVEVQC.MOS_LIVE_VQC;
 
-
 for i=1:1000
+    tic
     [trainInd,valInd,testInd] = dividerand(585,Training_Percentage,0,Test_Percentaje); %Divido aleatoriamente el 80% de la base de datos
     %para entrenamiento
     
@@ -34,7 +41,7 @@ for i=1:1000
         'OptimizeHyperparameters',...
         {'BoxConstraint', 'Epsilon', 'KernelFunction'},...
         'CacheSize','maximal',...
-        'HyperparameterOptimizationOptions',struct('UseParallel',1,'MaxObjectiveEvaluations',35,...
+        'HyperparameterOptimizationOptions',struct('UseParallel',1,'MaxObjectiveEvaluations',SVR_Iterations,...
         'ShowPlots',false));
     
     yfit_LIVE= predict(Mdl,Test_Data);
@@ -53,6 +60,7 @@ for i=1:1000
     max(Spearman)
     max(Kendall_COrrelation)
     min(RMSE)
+    toc
 end
 
 
