ba699ea6c8941686443585b84a34935d5c7ec4ad
[roger nieto] <rogergomez@utp.edu.co>
2019-10-18 19:01:52 -0500

    create Distort_Video function

--- Calculando_Tiempo_HOG.m
+++ Calculando_Tiempo_HOG.m
@@ -8,6 +8,7 @@ img = imread('cameraman.tif');
 img2= imread('C:\Users\DeepLearning_PUJ\Downloads\VideoBLIINDS_Code_MicheleSaad\DSVD_test_Sequences\001Pri_IndPW_FQ_C4\img\0001.jpg');
 tic
 [featureVector,hogVisualization] = extractHOGFeatures(img2);
+toc
 
 figure;
 imshow(img2); 
